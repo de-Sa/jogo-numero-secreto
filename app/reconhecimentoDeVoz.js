0cf1d7f0d8e36ac84485a8e325b47f31e360cc97
de-Sa <lucas.desa10@outlook.com>
2024-03-06 22:02:19 -0300

    Commit de Incrementação

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,24 +1,25 @@
-const elementoChute = document.getElementById('chute')
+const elementoChute = document.getElementById("chute");
 
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-recognition.lang = 'pt-Br'
-recognition.start()
+recognition.lang = "pt-Br";
+recognition.start();
 
-recognition.addEventListener('result', onSpeak)
+recognition.addEventListener("result", onSpeak);
 
 function onSpeak(e) {
-    chute = e.results[0][0].transcript
-    exibirChuteNaTela(chute);
-    verificarOValorDoChute(chute);
+  chute = e.results[0][0].transcript;
+  exibirChuteNaTela(chute);
+  verificarOValorDoChute(chute);
 }
 
 function exibirChuteNaTela(chute) {
-    elementoChute.innerHTML = `
+  elementoChute.innerHTML = `
         <div>Você disse</div>
         <span class="box">${chute}</span>
-    `
+    `;
+  
 }
 
-recognition.addEventListener('end', () => recognition.start())
+recognition.addEventListener("end", () => recognition.start());
--- app/validacao.js
+++ app/validacao.js
@@ -23,19 +23,22 @@ function verificarOValorDoChute(chute) {
         elementoChute.innerHTML += `<div>valor inválido: Fale um numero entre ${menorValor} e ${maiorValor}</div>`
         return
     }
-    
-    if (numero === numeroSecreto) {
-        document.body.innerHTML = `
-        <h2>Você acertou!!</h2>
-        <h3>O número secreto era ${numeroSecreto}</h3>
 
-        <button id="jogar-novamente" class="botao-jogar">Jogar novamente</button>
-        `
-    }  else if (numero > numeroSecreto) {
-        elementoChute.innerHTML += `<div>O número é menor <i class="fa-solid fa-arrow-down"></i></div> `
-    } else {
-        elementoChute.innerHTML += `<div>O número é maior <i class="fa-solid fa-arrow-up"></i></div> `
-    }
+    switch (true) {
+        case (numero === numeroSecreto):
+          document.body.innerHTML = `
+            <h2>Você acertou!!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="botao-jogar">Jogar novamente</button>
+          `;
+          break;
+        case (numero > numeroSecreto):
+          elementoChute.innerHTML += `<div>O número é menor <i class="fa-solid fa-arrow-down"></i></div>`;
+          break;
+        default:
+          elementoChute.innerHTML += `<div>O número é maior <i class="fa-solid fa-arrow-up"></i></div>`;
+          break;
+      }
 
 }
 
